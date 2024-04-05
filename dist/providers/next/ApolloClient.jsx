57e2e841837b21ce0e47ef85f2149bbaff2ca2c1
Vann Liu <vann@cpcp.tw>
2024-04-05 15:53:22 +0800

    build

--- dist/providers/next/ApolloClient.jsx
+++ dist/providers/next/ApolloClient.jsx
@@ -1,23 +1,15 @@
 "use client";
 import { ApolloNextAppProvider } from "@apollo/experimental-nextjs-app-support/ssr";
 import { loadErrorMessages, loadDevMessages } from "@apollo/client/dev";
-import useLangGuard from "../../use/next/useLangGuard";
 import { makeApolloClient } from "../../lib/next/apollo";
 if (process.env.NODE_ENV === 'development') {
     loadDevMessages();
     loadErrorMessages();
 }
 export default function ApolloClientProvider(_a) {
-    var children = _a.children;
-    var localeCode = useLangGuard().localeCode;
+    var children = _a.children, makeClient = _a.makeClient;
     return <ApolloNextAppProvider makeClient={function () {
-            return makeApolloClient({
-                context: {
-                    headers: {
-                        "accept-language": localeCode
-                    }
-                }
-            });
+            return (makeClient === null || makeClient === void 0 ? void 0 : makeClient()) || makeApolloClient();
         }}>
     {children}
   </ApolloNextAppProvider>;
