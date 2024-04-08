291906132bcb761ec349ad8734d288314073ddba
Vann Liu <vann@cpcp.tw>
2024-04-08 19:11:38 +0800

    fix: export

--- src/use/next/useLangGuard.ts
+++ src/use/next/useLangGuard.ts
@@ -1,5 +1,3 @@
-"use client"
-
 import { useCallback, useEffect } from "react"
 import { useParams, useRouter } from "next/navigation"
 import { usePathnameWithoutLang } from "./usePathnameWithoutLang"
