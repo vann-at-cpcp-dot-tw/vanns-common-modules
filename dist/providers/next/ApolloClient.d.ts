01bb4c5f2a44d89897c8cd31e39a00743033db17
Vann Liu <vann@cpcp.tw>
2024-04-09 11:31:28 +0800

    remove: useless

--- dist/providers/next/ApolloClient.d.ts
+++
@@ -1,5 +0,0 @@
-import { ReactNode } from "react";
-export declare function ApolloClientProvider({ children, makeClient, }: {
-    children: ReactNode;
-    makeClient: Function;
-}): import("react").JSX.Element;
--- dist/providers/next/index.js
+++ dist/providers/next/index.js
@@ -1,1 +1,1 @@
-export * from './ApolloClient';
+"use strict";
