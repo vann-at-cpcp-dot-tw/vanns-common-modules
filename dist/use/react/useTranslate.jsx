146b579ae46b21e14925d8b7e28c0be880b16a72
Vann Liu <vann@cpcp.tw>
2024-04-10 11:56:49 +0800

    fix: package

--- dist/use/react/useTranslate.jsx
+++ dist/use/react/useTranslate.jsx
@@ -1,6 +1,8 @@
-import React, { useCallback, useContext } from "react";
+import { useCallback, useContext } from "react";
 import { isEmpty } from "../../lib/helpers";
 import { TranslateContext } from "../../providers/react/Translate";
+// TODO： 因為 HtmlReactParser 的 bundle size 較高，加上翻譯檔是來自編輯後台（非開放平台），所以大膽採用 dangerouslySetInnerHTML 的方式
+import HtmlReactParser from 'html-react-parser';
 export function useTranslate() {
     var translationDoc = useContext(TranslateContext);
     var __ = useCallback(function (sourceString, params) {
@@ -17,7 +19,7 @@ export function useTranslate() {
         if (!Array.isArray(params)) {
             return translatedString;
         }
-        var result;
+        var result = '';
         if (Array.isArray(params)) {
             params.forEach(function (valueNode, i) {
                 if (['string', 'number'].includes(typeof valueNode)) {
@@ -27,13 +29,13 @@ export function useTranslate() {
                     var _a = valueNode !== null && valueNode !== void 0 ? valueNode : {}, value = _a.value, className = _a.className, style = _a.style;
                     if (value) {
                         result = translatedString.replaceAll("[s".concat(i + 1, "]"), "<span class=\"".concat(className, "\" style=\"").concat(style, "\">").concat(value, "</span>"));
-                        result = <span dangerouslySetInnerHTML={{ __html: result }}></span>;
+                        // result = <span dangerouslySetInnerHTML={{__html:result}}></span>
                     }
                 }
             });
         }
-        return result;
-        // return HtmlReactParser(result)
+        // return result
+        return HtmlReactParser(result);
     }, [translationDoc]);
     return {
         __: __
--- src/use/react/useTranslate.tsx
+++ src/use/react/useTranslate.tsx
@@ -2,7 +2,7 @@ import React, { useCallback, useContext, ReactNode } from "react"
 import { isEmpty } from "~/lib/helpers"
 import { TranslateContext } from "~/providers/react/Translate"
 // TODO： 因為 HtmlReactParser 的 bundle size 較高，加上翻譯檔是來自編輯後台（非開放平台），所以大膽採用 dangerouslySetInnerHTML 的方式
-// import HtmlReactParser from 'html-react-parser'
+import HtmlReactParser from 'html-react-parser'
 
 type IParamNode = {
   value: unknown,
@@ -32,7 +32,7 @@ export function useTranslate(){
       return translatedString
     }
 
-    let result
+    let result = ''
     if (Array.isArray(params)){
       params.forEach((valueNode, i)=>{
         if( ['string', 'number'].includes(typeof valueNode) ){
@@ -41,14 +41,14 @@ export function useTranslate(){
           const { value, className, style } = valueNode ?? {}
           if( value ){
             result = translatedString.replaceAll(`[s${i + 1}]`, `<span class="${className}" style="${style}">${value}</span>`)
-            result = <span dangerouslySetInnerHTML={{__html:result}}></span>
+            // result = <span dangerouslySetInnerHTML={{__html:result}}></span>
           }
         }
       })
     }
 
-    return result
-    // return HtmlReactParser(result)
+    // return result
+    return HtmlReactParser(result)
 
   }, [translationDoc])
 
