2cd188f3179f6e47a71df1fa482cfe79b89d8756
Vann Liu <vann@cpcp.tw>
2024-04-10 14:54:22 +0800

    fix

--- dist/use/react/useTranslate.d.ts
+++ dist/use/react/useTranslate.d.ts
@@ -1,10 +1,9 @@
-import React from "react";
 type IParamNode = {
     value: unknown;
     className?: string;
     style?: Record<string, string>;
 };
 export declare function useTranslate(): {
-    __: (sourceString: string, params?: (IParamNode | string | number)[]) => string | React.JSX.Element | React.JSX.Element[];
+    __: (sourceString: string, params?: (IParamNode | string | number)[]) => string | undefined;
 };
 export {};
--- dist/use/react/useTranslate.js
+++ dist/use/react/useTranslate.js
@@ -1,8 +1,7 @@
+import { jsx as _jsx } from "react/jsx-runtime";
 import { useCallback, useContext } from "react";
 import { isEmpty } from "../../lib/helpers";
 import { TranslateContext } from "../../providers/react/Translate";
-// TODO： 因為 HtmlReactParser 的 bundle size 較高，加上翻譯檔是來自編輯後台（非開放平台），所以大膽採用 dangerouslySetInnerHTML 的方式
-import HtmlReactParser from 'html-react-parser';
 export function useTranslate() {
     var translationDoc = useContext(TranslateContext);
     var __ = useCallback(function (sourceString, params) {
@@ -19,7 +18,7 @@ export function useTranslate() {
         if (!Array.isArray(params)) {
             return translatedString;
         }
-        var result = '';
+        var result;
         if (Array.isArray(params)) {
             params.forEach(function (valueNode, i) {
                 if (['string', 'number'].includes(typeof valueNode)) {
@@ -29,13 +28,13 @@ export function useTranslate() {
                     var _a = valueNode !== null && valueNode !== void 0 ? valueNode : {}, value = _a.value, className = _a.className, style = _a.style;
                     if (value) {
                         result = translatedString.replaceAll("[s".concat(i + 1, "]"), "<span class=\"".concat(className, "\" style=\"").concat(style, "\">").concat(value, "</span>"));
-                        // result = <span dangerouslySetInnerHTML={{__html:result}}></span>
+                        result = _jsx("span", { dangerouslySetInnerHTML: { __html: result } });
                     }
                 }
             });
         }
-        // return result
-        return HtmlReactParser(result);
+        return result;
+        // return HtmlReactParser(result)
     }, [translationDoc]);
     return {
         __: __
