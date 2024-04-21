6adfc6906f8dfd34c0c10094e9a6104a507862e2
Vann Liu <vann@cpcp.tw>
2024-04-21 18:43:33 +0800

    make scope store freedom

--- dist/providers/react/ScopeStore.d.ts
+++ dist/providers/react/ScopeStore.d.ts
@@ -1,8 +1,38 @@
 import { ReactNode } from 'react';
-export declare const ScopeStoreContext: import("react").Context<{
-    [key: string]: any;
-}>;
-export declare function ScopeStoreProvider({ children, state }: {
-    children: ReactNode;
-    state: any;
-}): import("react/jsx-runtime").JSX.Element;
+export declare function createScopeStoreProvider<T>(): {
+    ScopeStoreProvider: ({ children, state }: {
+        children: ReactNode;
+        state: T;
+    }) => import("react/jsx-runtime").JSX.Element;
+    useScopeStore: () => NonNullable<T>;
+};
+/****************** 使用方法 ******************
+
+
+//=================  創建方  =================//
+// 定義 state 的類型
+interface MyState {
+  count: number
+  setCount: () => void
+  // ...其他屬性
+}
+
+// 創建一個新的 Provider 組件
+export const { ScopeStoreProvider, useScopeStore } = createScopeStoreProvider<MyState>()
+const [count, setCount] = useState(0)
+<ScopeStoreProvider state={{
+  count,
+  setCount
+}}>
+  <ChildrenComponent />
+</ScopeStoreProvider>
+
+
+
+//=================  使用方  =================//
+import { useScopeStore } from '/path/to/export'
+const scopeStore = useScopeStore()
+console.log(scopeStore.count)
+
+
+****************** 使用方法 ******************/
--- dist/providers/react/ScopeStore.js
+++ dist/providers/react/ScopeStore.js
@@ -1,8 +1,21 @@
-"use client";
 import { jsx as _jsx } from "react/jsx-runtime";
-import { createContext } from 'react';
-export var ScopeStoreContext = createContext({});
-export function ScopeStoreProvider(_a) {
-    var children = _a.children, state = _a.state;
-    return _jsx(ScopeStoreContext.Provider, { value: state, children: children });
+import { createContext, useContext } from 'react';
+// 這是一個泛型函數, 用於創建 Provider 組件
+export function createScopeStoreProvider() {
+    // 創建一個新的 context
+    var ScopeStoreContext = createContext(undefined);
+    // Provider 組件
+    var ScopeStoreProvider = function (_a) {
+        var children = _a.children, state = _a.state;
+        return _jsx(ScopeStoreContext.Provider, { value: state, children: children });
+    };
+    // 一個自訂的 hook,用於獲取 context 的值
+    var useScopeStore = function () {
+        var context = useContext(ScopeStoreContext);
+        if (!context) {
+            throw new Error('useScopeStore must be used within a ScopeStoreProvider');
+        }
+        return context;
+    };
+    return { ScopeStoreProvider: ScopeStoreProvider, useScopeStore: useScopeStore };
 }
