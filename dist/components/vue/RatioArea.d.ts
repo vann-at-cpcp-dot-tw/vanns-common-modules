2d37d3be71ed05f9ba338996a040fa37e848eeb7
Vann Liu <vann@cpcp.tw>
2024-05-01 22:09:14 +0800

    feat: add vue components

---
+++ dist/components/vue/RatioArea.d.ts
@@ -0,0 +1,22 @@
+declare const _default: import("vue").DefineComponent<{
+    className: {
+        type: StringConstructor;
+        default: string;
+    };
+    ratio: {
+        type: (StringConstructor | NumberConstructor)[];
+        required: true;
+    };
+}, () => import("react/jsx-runtime").JSX.Element, unknown, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {}, string, import("vue").PublicProps, Readonly<import("vue").ExtractPropTypes<{
+    className: {
+        type: StringConstructor;
+        default: string;
+    };
+    ratio: {
+        type: (StringConstructor | NumberConstructor)[];
+        required: true;
+    };
+}>>, {
+    className: string;
+}, {}>;
+export default _default;
---
+++ src/components/vue/RatioArea.tsx
@@ -0,0 +1,26 @@
+import { ReactNode } from "react"
+import { twMerge } from 'tailwind-merge'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    className: {
+      type: String,
+      default: '',
+    },
+    ratio: {
+      type: [Number, String],
+      required: true,
+    }
+  },
+  setup(props, {emit, slots}){
+    return ()=> <div className={twMerge('ratioArea relative w-full', props?.className)}>
+    <div className="fill pointer-events-none relative"
+    style={{
+      width: '100%',
+      paddingBottom: `${props.ratio}%`
+    }}></div>
+    { (slots?.default?.() as ReactNode) }
+  </div>
+  }
+})
