fe411d6c852d249b603701a3e21c1e1c6e3c795d
Vann Liu <vann@cpcp.tw>
2024-05-15 17:59:42 +0800

    fix: logic pathname start with lang

--- dist/use/next/usePathnameWithoutLang.js
+++ dist/use/next/usePathnameWithoutLang.js
@@ -1,10 +1,14 @@
 import { useMemo } from "react";
 import { useParams, usePathname } from "next/navigation";
+function isPathStartingWithLang(path, lang) {
+    var pattern = new RegExp("^/".concat(lang, "(?:/|\\?|$)"));
+    return pattern.test(path);
+}
 export function usePathnameWithoutLang() {
     var lang = useParams().lang;
     var pathname = usePathname();
     var pathnameWithoutLang = useMemo(function () {
-        return pathname.startsWith("/".concat(lang, "/")) ? pathname.replace("/".concat(lang), '') : pathname;
+        return isPathStartingWithLang(pathname, lang) ? pathname.replace("/".concat(lang), '') : pathname;
     }, [pathname, lang]);
     return pathnameWithoutLang;
 }
--- src/use/next/usePathnameWithoutLang.ts
+++ src/use/next/usePathnameWithoutLang.ts
@@ -1,11 +1,16 @@
 import { useMemo } from "react"
 import { useParams, usePathname } from "next/navigation"
 
+function isPathStartingWithLang(path:string, lang:string) {
+  const pattern = new RegExp(`^/${lang}(?:/|\\?|$)`);
+  return pattern.test(path);
+}
+
 export function usePathnameWithoutLang(){
   const { lang } = useParams()
   const pathname = usePathname()
   const pathnameWithoutLang = useMemo(()=>{
-    return pathname.startsWith(`/${lang}/`) ?pathname.replace(`/${lang}`, '') :pathname
+    return isPathStartingWithLang(pathname, lang as string) ?pathname.replace(`/${lang}`, '') :pathname
   }, [pathname, lang])
 
   return pathnameWithoutLang
