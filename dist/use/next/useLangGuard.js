64b4a45fb306daac89ac52255b7dce4ded606b52
Vann Liu <vann@cpcp.tw>
2024-05-15 18:04:14 +0800

    fix: logic useLangGuard

--- dist/use/next/useLangGuard.js
+++ dist/use/next/useLangGuard.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect } from "react";
+import { useEffect } from "react";
 import { useParams, useRouter } from "next/navigation";
 import { usePathnameWithoutLang } from "../../use/next/usePathnameWithoutLang";
 import { useSearchObject } from "../../use/next";
@@ -38,43 +38,14 @@ export function useLangGuard(i18nConfig) {
     var lang = params.lang;
     var _a = tools(i18nConfig), convertLocaleCode = _a.convertLocaleCode, pathnameWithLang = _a.pathnameWithLang, isSupportedLang = _a.isSupportedLang;
     var localeCode = convertLocaleCode(lang, 'long');
-    var determineTargetPath = useCallback(function (storedLang, browserLang, path) {
-        // 語言優先度： URL夾帶 > localStorage 儲存 > browser lang > default lang
-        // 如果 URL 有傳 lang，且不等於 default lang（網址有明確夾帶 lang）的情況...
-        if (lang && typeof lang === 'string' && lang !== i18nConfig.defaultLocale.shortCode) {
-            var targetLang = i18nConfig.defaultLocale.shortCode;
-            if (isSupportedLang(lang)) {
-                targetLang = lang;
-            }
-            else if (storedLang && isSupportedLang(storedLang)) {
-                targetLang = storedLang;
-            }
-            return pathnameWithLang(path, targetLang);
-        }
-        // 如果 localStorage 有儲存 lang
-        if (storedLang) {
-            // 如果儲存的 lang 和 URL 不匹配，則回傳 storedLang 或 default lang
-            if (storedLang !== lang) {
-                return pathnameWithLang(path, isSupportedLang(storedLang) ? storedLang : i18nConfig.defaultLocale.shortCode);
-            }
-            // 如果 localStorage 沒有儲存 lang，但瀏覽器的 lang 在網站支援的語言列表中，則自動將瀏覽器語言儲存至 localStorage 後，回傳 browserLang
-        }
-        else if (isSupportedLang(browserLang)) {
-            localStorage.setItem('lang', browserLang);
-            return pathnameWithLang(path, browserLang);
-            // 如果 localStorage 沒有儲存 lang，且瀏覽器的 lang 又不在網站支援的語言列表中，則回傳 default lang
-        }
-        else {
-            localStorage.setItem('lang', i18nConfig.defaultLocale.shortCode);
-            return pathnameWithLang(path, i18nConfig.defaultLocale.shortCode);
-        }
-    }, [lang]);
     useEffect(function () {
         var browserLocaleCode = navigator.language;
         var path = pathname;
         var storedLang = localStorage.getItem('lang');
         var browserLang = convertLocaleCode(browserLocaleCode, 'short') || i18nConfig.defaultLocale.shortCode;
-        var targetPath = determineTargetPath(storedLang, browserLang, path);
+        var targetLang = lang || storedLang || browserLang;
+        targetLang = isSupportedLang(targetLang) ? targetLang : i18nConfig.defaultLocale.shortCode;
+        var targetPath = pathnameWithLang(path, targetLang);
         if (targetPath) {
             router.push("".concat(targetPath, "?").concat(searchString));
         }
--- src/use/next/useLangGuard.ts
+++ src/use/next/useLangGuard.ts
@@ -47,51 +47,15 @@ export function useLangGuard(i18nConfig:TypeI18n){
   const { convertLocaleCode, pathnameWithLang, isSupportedLang } = tools(i18nConfig)
   const localeCode = convertLocaleCode((lang as string), 'long')
 
-  const determineTargetPath = useCallback((storedLang:string | null, browserLang:string, path:string)=>{
-    // 語言優先度： URL夾帶 > localStorage 儲存 > browser lang > default lang
-    // 如果 URL 有傳 lang，且不等於 default lang（網址有明確夾帶 lang）的情況...
-    if( lang && typeof lang === 'string' && lang !== i18nConfig.defaultLocale.shortCode ){
-
-      let targetLang = i18nConfig.defaultLocale.shortCode
-
-      if( isSupportedLang(lang) ){
-        targetLang = lang
-      }else if( storedLang && isSupportedLang(storedLang) ){
-        targetLang = storedLang
-      }
-
-      return pathnameWithLang(path, targetLang)
-    }
-
-    // 如果 localStorage 有儲存 lang
-    if (storedLang ) {
-
-      // 如果儲存的 lang 和 URL 不匹配，則回傳 storedLang 或 default lang
-      if( storedLang !== lang ){
-        return pathnameWithLang(path, isSupportedLang(storedLang) ? storedLang : i18nConfig.defaultLocale.shortCode)
-      }
-
-    // 如果 localStorage 沒有儲存 lang，但瀏覽器的 lang 在網站支援的語言列表中，則自動將瀏覽器語言儲存至 localStorage 後，回傳 browserLang
-    } else if (isSupportedLang(browserLang)) {
-
-      localStorage.setItem('lang', browserLang)
-      return pathnameWithLang(path, browserLang)
-
-    // 如果 localStorage 沒有儲存 lang，且瀏覽器的 lang 又不在網站支援的語言列表中，則回傳 default lang
-    } else {
-      localStorage.setItem('lang', i18nConfig.defaultLocale.shortCode)
-      return pathnameWithLang(path, i18nConfig.defaultLocale.shortCode)
-    }
-  }, [lang])
-
   useEffect(()=>{
 
     const browserLocaleCode = navigator.language
     const path = pathname
     const storedLang = localStorage.getItem('lang')
     const browserLang = convertLocaleCode(browserLocaleCode, 'short') || i18nConfig.defaultLocale.shortCode
-    const targetPath = determineTargetPath(storedLang, browserLang, path)
-
+    let targetLang = lang || storedLang || browserLang
+    targetLang = isSupportedLang(targetLang as string) ? targetLang : i18nConfig.defaultLocale.shortCode
+    const targetPath = pathnameWithLang(path, targetLang as string)
     if (targetPath) {
       router.push(`${targetPath}?${searchString}`)
     }
