19a9279fed574781c8fe99f8a963dde62b81b0cc
Vann Liu <vann@cpcp.tw>
2024-05-16 11:08:00 +0800

    fix: logic

--- dist/use/next/usePathnameWithoutLang.js
+++ dist/use/next/usePathnameWithoutLang.js
@@ -8,7 +8,12 @@ export function usePathnameWithoutLang() {
     var lang = useParams().lang;
     var pathname = usePathname();
     var pathnameWithoutLang = useMemo(function () {
-        return isPathnameStartWithLang(pathname, lang) ? pathname.replace("/".concat(lang), '') : pathname;
+        var result = pathname;
+        if (isPathnameStartWithLang(pathname, lang)) {
+            // 如果 pathname 為根節點(首頁)的話，replace 掉 lang 會變空字串，改為 return '/'
+            result = pathname.replace("/".concat(lang), '') || '/';
+        }
+        return result;
     }, [pathname, lang]);
     return pathnameWithoutLang;
 }
--- src/use/next/usePathnameWithoutLang.ts
+++ src/use/next/usePathnameWithoutLang.ts
@@ -10,7 +10,12 @@ export function usePathnameWithoutLang(){
   const { lang } = useParams()
   const pathname = usePathname()
   const pathnameWithoutLang = useMemo(()=>{
-    return isPathnameStartWithLang(pathname, lang as string) ?pathname.replace(`/${lang}`, '') :pathname
+    let result = pathname
+    if( isPathnameStartWithLang(pathname, lang as string) ){
+      // 如果 pathname 為根節點(首頁)的話，replace 掉 lang 會變空字串，改為 return '/'
+      result = pathname.replace(`/${lang}`, '') || '/'
+    }
+    return result
   }, [pathname, lang])
 
   return pathnameWithoutLang
