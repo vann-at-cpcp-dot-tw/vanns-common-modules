964cff15350e97da8623776b3c35a8b97aada21d
Vann Liu <vann@cpcp.tw>
2024-08-21 10:54:23 +0800

    fix: translate component support HTML - build

--- dist/components/react/T.jsx
+++ dist/components/react/T.jsx
@@ -1,11 +1,18 @@
 "use client";
-import { Suspense } from 'react';
+import { Suspense, useMemo } from 'react';
 import { twMerge } from 'tailwind-merge';
 import { useTranslate } from "../../use/react";
 export default function Translate(props, ref) {
     var _a = props !== null && props !== void 0 ? props : {}, id = _a.id, className = _a.className;
     var __ = useTranslate().__;
+    var result = useMemo(function () {
+        return __(props.text, props === null || props === void 0 ? void 0 : props.vars);
+    }, [props.text, props === null || props === void 0 ? void 0 : props.vars, __]);
     return <Suspense fallback={null}>
-    <span className={twMerge('', className)}>{__(props.text, props === null || props === void 0 ? void 0 : props.vars)}</span>
+    <span className={twMerge('', className)}>
+      {typeof result === 'string'
+            ? <span dangerouslySetInnerHTML={{ __html: __(props.text, props === null || props === void 0 ? void 0 : props.vars) || '' }}></span>
+            : <span>{__(props.text, props === null || props === void 0 ? void 0 : props.vars)}</span>}
+    </span>
   </Suspense>;
 }
