1fe1b03d13e3bb5f5c5e9cb1b4096c89b16f8f7a
Vann Liu <vann@cpcp.tw>
2024-08-21 10:52:05 +0800

    fix: translate component support HTML

--- src/components/react/T.tsx
+++ src/components/react/T.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Suspense } from 'react'
+import { Suspense, useMemo } from 'react'
 
 import { twMerge } from 'tailwind-merge'
 import { useTranslate, IParamNode } from "~/use/react"
@@ -15,7 +15,16 @@ interface IProps {
 export default function Translate(props:IProps, ref:React.ReactNode){
   const { id, className } = props ?? {}
   const { __ } = useTranslate()
+  const result = useMemo(()=>{
+    return __(props.text, props?.vars)
+  }, [props.text, props?.vars, __])
   return <Suspense fallback={null}>
-    <span className={twMerge('', className)}>{ __(props.text, props?.vars) }</span>
+    <span className={twMerge('', className)}>
+      {
+        typeof result === 'string'
+        ? <span dangerouslySetInnerHTML={{ __html: __(props.text, props?.vars) || '' }}></span>
+        : <span>{ __(props.text, props?.vars) }</span>
+      }
+    </span>
   </Suspense>
 }
