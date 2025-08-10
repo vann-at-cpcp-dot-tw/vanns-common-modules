4f92678beb06f2f85296d56738292b70e1275e4e
vann-at-cpcp-dot-tw <vann@cpcp.tw>
2025-08-10 17:55:55 +0800

    fix

--- src/lib/next/apollo/client.tsx
+++ src/lib/next/apollo/client.tsx
@@ -10,8 +10,7 @@ if (process.env.NODE_ENV === 'development') {
   loadErrorMessages()
 }
 
-// TODO: 此處的 makeClient 和 ./server.ts 裡的 makeClient 為重複 code，曾經嘗試整合兩者(依賴傳入參數決定要 return 哪種 apollo client)，但因 "@apollo/experimental-nextjs-app-support/ssr" module 在 next.js 這種 client/server 混合式框架下，會報錯，當在 server component 下要引入 NextSSRApolloClient，會提示引入不到，反之亦然，所以只能暫時使用重複 code 解決
-// 錯誤訊息：Attempted import error: 'NextSSRApolloClient' is not exported from '@apollo/experimental-nextjs-app-support/ssr' (imported as 'NextSSRApolloClient').
+// TODO: 此處的 makeClient 和 ./server.ts 裡的 makeClient 為重複 code，曾經嘗試整合兩者(依賴傳入參數決定要 return 哪種 apollo client)，但因 client/server 兩種環境下的 apollo client 實作不同而需要分開處理，暫時使用重複 code 解決
 
 export function makeApolloClient(args?:IMakeApolloClient){
 
